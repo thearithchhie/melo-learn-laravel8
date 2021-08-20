f7ab586890c33e8f78a04643aa993c8048166569
thearithchhie <thearithchhie@gmail.com>
2021-08-20 17:03:02 +0700

    update middleware

--- routes/web.php
+++ routes/web.php
@@ -17,17 +17,17 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 Route::get('/users', [UserController::class, 'show']);
 
 Route::group(['middleware' => 'auth'], function () {
+
     Route::prefix("category")->group(function () {
         Route::get('show', [CategoryController::class, 'show'])->name("show.category");
         Route::get("add",[CategoryController::class, 'CategoryAdd'])->name("category.add");
         Route::post("store",[CategoryController::class, 'CategoryStore'])->name("category.store");
     });
-});
-
-Route::group(['middleware' => 'auth'], function () {
+    
     Route::prefix("post")->group(function () {
         Route::get('/show', [PostController::class, 'show']);
     });
+
 });
 
 Route::group(['middleware' => 'auth'], function () {
